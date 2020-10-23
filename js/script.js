3b4b9f6fd4d9a21ca32c4fd8fc788d38d59f463f
Gustavo Becelli do Nacimento <gustavobecelli@pop-os.localdomain>
2020-10-23 02:42:17 -0300

    feat: added header, with photo and infos about me.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
-var width = window.client.width;
-alert(width)
+
+function viewOnGithubIn() {
+    // add blur and change z-index
+    const image = document.getElementById("profile-photo")
+    const text = document.getElementById("text-over-image")
+    image.classList.add("blurred")
+    text.classList.add("index-show");
+}
+function viewOnGithubOut() {
+    const image = document.getElementById("profile-photo")
+    const text = document.getElementById("text-over-image")
+    image.classList.remove("blurred")
+    text.classList.remove("index-show");
+    // remove blur and change z-index
+}
+ 
