609b01edc2113e6ea1c83a43508ac58d9400e4ab
Gustavo Becelli do Nacimento <gustavobecelli@pop-os.localdomain>
2020-10-25 21:47:02 -0300

    improved  mobile experience and created posts page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 // ARE YOU READING THIS? YOU'RE REALLY INTERESTED! LOL
+const width = window.innerWidth;
+const numberOfCards = 3;
+function resizeCards(){
+    if(width < 540){
 
+        for(let i = 0; i < numberOfCards; i++){
+        const resizeHeight = document.querySelectorAll(".my-card")[i]
+        resizeHeight.style.height = "500px"
+        resizeHeight.style.border = "0.5px solid whitesmoke"
+        }
+    }
+}
 
 function viewOnGithubIn() {
     // add blur and change z-index
@@ -14,6 +25,4 @@ function viewOnGithubOut() {
     const text = document.getElementById("text-over-image")
     image.classList.remove("blurred")
     text.classList.remove("index-show");
-
 }
- 
