beedba3b6aa993cf1bfad59a2f33b166a802c159
Gustavo Becelli do Nacimento <gustavobecelli@pop-os.localdomain>
2020-10-26 21:22:26 -0300

    feat: resize cards by width of the screen

--- js/index.js
+++ js/index.js
@@ -1,12 +1,39 @@
 // ARE YOU READING THIS? YOU'RE REALLY INTERESTED! LOL
 const width = window.innerWidth
+const height = window.innerHeight
 const numberOfCards = 3
 
+
+
+// function resizeCards(){
+//     if(width < 540){
+//         for(let i = 0; i < numberOfCards; i++){
+//             const resizeHeight = document.querySelectorAll(".my-card")[i]
+//             resizeHeight.style.height = "500px"
+//             resizeHeight.style.border = "0.5px solid whitesmoke"
+//         }
+//     }
+//     else {
+//         const headerHeight = document.getElementById("header").offsetHeight
+//         const footerHeight = document.querySelector(".footer").offsetHeight
+//         alert(headerHeight + " and " + footerHeight)
+//         const
+//     }
+// }
 function resizeCards(){
+    var newWidth, mobile = 0;
     if(width < 540){
-        for(let i = 0; i < numberOfCards; i++){
-            const resizeHeight = document.querySelectorAll(".my-card")[i]
-            resizeHeight.style.height = "500px"
+        newWidth = 500;
+        mobile = 1;
+    } else if(width > 992){
+        const headerHeight = document.getElementById("header").offsetHeight
+        const footerHeight = document.querySelector(".footer").offsetHeight
+        newWidth = height - (headerHeight + footerHeight)
+    }
+    for(let i = 0; i < numberOfCards; i++){
+        const resizeHeight = document.querySelectorAll(".my-card")[i]
+        resizeHeight.style.height = newWidth + "px"
+        if(mobile){
             resizeHeight.style.border = "0.5px solid whitesmoke"
         }
     }
